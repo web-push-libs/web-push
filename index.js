148a6597727aa10bfdf50e0c6d777b5cf0751f35
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-29 20:49:03 +0200

    Add sendNotification function

--- index.js
+++ index.js
@@ -26,6 +26,39 @@ function encrypt(userPublicKey, payload) {
   };
 }
 
+function sendNotification(endpoint, userPublicKey, payload) {
+  var encrypted = webPush.encrypt(urlBase64.decode(userPublicKey), payload);
+
+  var urlParts = url.parse(endpoint);
+  var options = {
+    hostname: urlParts.hostname,
+    path: urlParts.pathname,
+    method: 'POST',
+    headers: {
+      'Content-Length': encrypted.cipherText.length,
+      'Content-Type': 'application/octet-stream',
+      'Encryption-Key': 'keyid=p256dh;dh=' + urlBase64.encode(encrypted.localPublicKey),
+      'Encryption': 'keyid=p256dh;salt=' + urlBase64.encode(encrypted.salt),
+      'Content-Encoding': 'aesgcm128',
+    },
+  };
+
+  var pushRequest = https.request(options, function(pushResponse) {
+    if (pushResponse.statusCode !== 201) {
+      console.log("statusCode: ", pushResponse.statusCode);
+      console.log("headers: ", pushResponse.headers);
+    }
+  });
+
+  pushRequest.write(encrypted.cipherText);
+  pushRequest.end();
+
+  pushRequest.on('error', function(e) {
+    console.error(e);
+  });
+}
+
 module.exports = {
   encrypt: encrypt,
+  sendNotification: sendNotification,
 }
