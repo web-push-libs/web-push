5880c424e5fb5a1fd5881c97dc2d7b437703251e
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-10-02 01:43:36 +0200

    Basic unit test for the setGCMAPIKey function

---
+++ test/testSetGCMAPIKey.js
@@ -0,0 +1,15 @@
+var assert    = require('assert');
+var crypto    = require('crypto');
+var webPush   = require('../index');
+var ece       = require('encrypted-content-encoding');
+var urlBase64 = require('urlsafe-base64');
+
+suite('setGCMAPIKey', function() {
+  test('is defined', function() {
+    assert(webPush.setGCMAPIKey);
+  });
+
+  test('call', function() {
+    webPush.setGCMAPIKey('hello');
+  })
+});
