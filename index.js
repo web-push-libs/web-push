77e763fed264d15682e8925a84861b81261cc0ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-03-18 15:34:15 +0100

    Reject promise if the endpoint isn't defined

--- index.js
+++ index.js
@@ -72,123 +72,129 @@ function encrypt(userPublicKey, userAuth, payload) {
 }
 
 function sendNotification(endpoint, params) {
-  if (arguments.length === 0) {
-    throw new Error('sendNotification requires at least one argument, the endpoint URL');
-  } else if (params && typeof params === 'object') {
-    var TTL = params.TTL;
-    var userPublicKey = params.userPublicKey;
-    var userAuth = params.userAuth;
-    var payload = params.payload;
-  } else {
-    var TTL = arguments[1];
-    var userPublicKey = arguments[2];
-    var payload = arguments[3];
-    console.warn('You are using the old, deprecated, interface of the `sendNotification` function.'.bold.red);
-  }
+  var args = arguments;
 
   return new Promise(function(resolve, reject) {
-    const isGCM = endpoint.indexOf('https://android.googleapis.com/gcm/send') === 0;
-
-    var urlParts = url.parse(endpoint);
-    var options = {
-      hostname: urlParts.hostname,
-      port: urlParts.port,
-      path: urlParts.pathname,
-      method: 'POST',
-      headers: {
-        'Content-Length': 0,
-      }
-    };
-
-    var encrypted;
-    if (typeof payload !== 'undefined') {
-      // Use Encryption-Key and the old standard for Firefox for now, otherwise we
-      // can't support Firefox 45.
-      // After Firefox 46 is released, we should switch to Crypto-Key, so we can
-      // support VAPID with notifications with payloads.
-      // We should take https://bugzilla.mozilla.org/show_bug.cgi?id=1257821 into
-      // account though.
-      const useNewStandard = isGCM;
-
-      encrypted = useNewStandard ? encrypt(userPublicKey, userAuth, new Buffer(payload)) :
-                                   encryptOld(userPublicKey, new Buffer(payload));
-
-      options.headers = {
-        'Content-Length': encrypted.cipherText.length,
-        'Content-Type': 'application/octet-stream',
-        'Encryption': 'keyid=p256dh;salt=' + urlBase64.encode(encrypted.salt),
-      };
-
-      var cryptoHeader = 'keyid=p256dh;dh=' + urlBase64.encode(encrypted.localPublicKey);
-
-      if (useNewStandard) {
-        options.headers['Crypto-Key'] = cryptoHeader;
-        options.headers['Content-Encoding'] = 'aesgcm';
+    try {
+      if (args.length === 0) {
+        throw new Error('sendNotification requires at least one argument, the endpoint URL');
+      } else if (params && typeof params === 'object') {
+        var TTL = params.TTL;
+        var userPublicKey = params.userPublicKey;
+        var userAuth = params.userAuth;
+        var payload = params.payload;
       } else {
-        options.headers['Encryption-Key'] = cryptoHeader;
-        options.headers['Content-Encoding'] = 'aesgcm128';
-      }
-    }
-
-    var gcmPayload;
-    if (isGCM) {
-      if (!gcmAPIKey) {
-        console.warn('Attempt to send push notification to GCM endpoint, but no GCM key is defined'.bold.red);
+        var TTL = args[1];
+        var userPublicKey = args[2];
+        var payload = args[3];
+        console.warn('You are using the old, deprecated, interface of the `sendNotification` function.'.bold.red);
       }
 
-      var endpointSections = endpoint.split('/');
-      var subscriptionId = endpointSections[endpointSections.length - 1];
+      const isGCM = endpoint.indexOf('https://android.googleapis.com/gcm/send') === 0;
 
-      var gcmObj = {
-        registration_ids: [ subscriptionId ],
-      };
-      if (encrypted) {
-        gcmObj['raw_data'] = urlBase64.encode(encrypted.cipherText);
-      }
-      gcmPayload = JSON.stringify(gcmObj);
-
-      options.path = options.path.substring(0, options.path.length - subscriptionId.length - 1);
-
-      options.headers['Authorization'] = 'key=' + gcmAPIKey;
-      options.headers['Content-Type'] = 'application/json';
-      options.headers['Content-Length'] = gcmPayload.length;
-    }
-
-    if (typeof TTL !== 'undefined') {
-      options.headers['TTL'] = TTL;
-    } else {
-      options.headers['TTL'] = 2419200; // Default TTL is four weeks.
-    }
-
-    var expectedStatusCode = isGCM ? 200 : 201;
-    var pushRequest = https.request(options, function(pushResponse) {
-      var body = "";
-
-      pushResponse.on('data', function(chunk) {
-        body += chunk;
-      });
-
-      pushResponse.on('end', function() {
-        if (pushResponse.statusCode !== expectedStatusCode) {
-          reject(new WebPushError('Received unexpected response code', pushResponse.statusCode, pushResponse.headers, body));
-        } else {
-          resolve(body);
+      var urlParts = url.parse(endpoint);
+      var options = {
+        hostname: urlParts.hostname,
+        port: urlParts.port,
+        path: urlParts.pathname,
+        method: 'POST',
+        headers: {
+          'Content-Length': 0,
         }
+      };
+
+      var encrypted;
+      if (typeof payload !== 'undefined') {
+        // Use Encryption-Key and the old standard for Firefox for now, otherwise we
+        // can't support Firefox 45.
+        // After Firefox 46 is released, we should switch to Crypto-Key, so we can
+        // support VAPID with notifications with payloads.
+        // We should take https://bugzilla.mozilla.org/show_bug.cgi?id=1257821 into
+        // account though.
+        const useNewStandard = isGCM;
+
+        encrypted = useNewStandard ? encrypt(userPublicKey, userAuth, new Buffer(payload)) :
+                                     encryptOld(userPublicKey, new Buffer(payload));
+
+        options.headers = {
+          'Content-Length': encrypted.cipherText.length,
+          'Content-Type': 'application/octet-stream',
+          'Encryption': 'keyid=p256dh;salt=' + urlBase64.encode(encrypted.salt),
+        };
+
+        var cryptoHeader = 'keyid=p256dh;dh=' + urlBase64.encode(encrypted.localPublicKey);
+
+        if (useNewStandard) {
+          options.headers['Crypto-Key'] = cryptoHeader;
+          options.headers['Content-Encoding'] = 'aesgcm';
+        } else {
+          options.headers['Encryption-Key'] = cryptoHeader;
+          options.headers['Content-Encoding'] = 'aesgcm128';
+        }
+      }
+
+      var gcmPayload;
+      if (isGCM) {
+        if (!gcmAPIKey) {
+          console.warn('Attempt to send push notification to GCM endpoint, but no GCM key is defined'.bold.red);
+        }
+
+        var endpointSections = endpoint.split('/');
+        var subscriptionId = endpointSections[endpointSections.length - 1];
+
+        var gcmObj = {
+          registration_ids: [ subscriptionId ],
+        };
+        if (encrypted) {
+          gcmObj['raw_data'] = urlBase64.encode(encrypted.cipherText);
+        }
+        gcmPayload = JSON.stringify(gcmObj);
+
+        options.path = options.path.substring(0, options.path.length - subscriptionId.length - 1);
+
+        options.headers['Authorization'] = 'key=' + gcmAPIKey;
+        options.headers['Content-Type'] = 'application/json';
+        options.headers['Content-Length'] = gcmPayload.length;
+      }
+
+      if (typeof TTL !== 'undefined') {
+        options.headers['TTL'] = TTL;
+      } else {
+        options.headers['TTL'] = 2419200; // Default TTL is four weeks.
+      }
+
+      var expectedStatusCode = isGCM ? 200 : 201;
+      var pushRequest = https.request(options, function(pushResponse) {
+        var body = "";
+
+        pushResponse.on('data', function(chunk) {
+          body += chunk;
+        });
+
+        pushResponse.on('end', function() {
+          if (pushResponse.statusCode !== expectedStatusCode) {
+            reject(new WebPushError('Received unexpected response code', pushResponse.statusCode, pushResponse.headers, body));
+          } else {
+            resolve(body);
+          }
+        });
       });
-    });
 
-    if (isGCM) {
-      pushRequest.write(gcmPayload);
-    } else if (typeof payload !== 'undefined') {
-      pushRequest.write(encrypted.cipherText);
-    }
+      if (isGCM) {
+        pushRequest.write(gcmPayload);
+      } else if (typeof payload !== 'undefined') {
+        pushRequest.write(encrypted.cipherText);
+      }
 
-    pushRequest.end();
+      pushRequest.end();
 
-    pushRequest.on('error', function(e) {
-      console.error(e);
+      pushRequest.on('error', function(e) {
+        console.error(e);
+        reject(e);
+      });
+    } catch (e) {
       reject(e);
-    });
+    }
   });
 }
 
