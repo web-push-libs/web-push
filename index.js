f63dae371ffb57f69cb8165f5e2b19b445f2d71d
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-03-18 13:51:53 +0100

    Support push payloads with GCM (using the new encryption standard)

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ function setGCMAPIKey(apiKey) {
   gcmAPIKey = apiKey;
 }
 
-function encrypt(userPublicKey, payload) {
+function encryptOld(userPublicKey, payload) {
   var localCurve = crypto.createECDH('prime256v1');
 
   var localPublicKey = localCurve.generateKeys();
   var localPrivateKey = localCurve.getPrivateKey();
 
-  var sharedSecret = localCurve.computeSecret(userPublicKey);
+  var sharedSecret = localCurve.computeSecret(urlBase64.decode(userPublicKey));
 
   var salt = crypto.randomBytes(16);
 
@@ -48,8 +48,33 @@ function encrypt(userPublicKey, payload) {
   };
 }
 
-function sendNotification(endpoint, TTL, userPublicKey, payload) {
+function encrypt(userPublicKey, userAuth, payload) {
+  var localCurve = crypto.createECDH('prime256v1');
+  var localPublicKey = localCurve.generateKeys();
+
+  var salt = crypto.randomBytes(16);
+
+  ece.saveKey('webpushKey', localCurve, 'P-256');
+
+  var cipherText = ece.encrypt(payload, {
+    keyid: 'webpushKey',
+    dh: userPublicKey,
+    salt: urlBase64.encode(salt),
+    authSecret: userAuth,
+    padSize: 2,
+  });
+
+  return {
+    localPublicKey: localPublicKey,
+    salt: salt,
+    cipherText: cipherText,
+  };
+}
+
+function sendNotification(endpoint, TTL, userPublicKey, userAuth, payload) {
   return new Promise(function(resolve, reject) {
+    const isGCM = endpoint.indexOf('https://android.googleapis.com/gcm/send') === 0;
+
     var urlParts = url.parse(endpoint);
     var options = {
       hostname: urlParts.hostname,
@@ -63,32 +88,51 @@ function sendNotification(endpoint, TTL, userPublicKey, payload) {
 
     var encrypted;
     if (typeof payload !== 'undefined') {
-      encrypted = encrypt(urlBase64.decode(userPublicKey), new Buffer(payload));
+      // Use Encryption-Key and the old standard for Firefox for now, otherwise we
+      // can't support Firefox 45.
+      // After Firefox 46 is released, we should switch to Crypto-Key, so we can
+      // support VAPID with notifications with payloads.
+      // We should take https://bugzilla.mozilla.org/show_bug.cgi?id=1257821 into
+      // account though.
+      const useNewStandard = isGCM;
+
+      encrypted = useNewStandard ? encrypt(userPublicKey, userAuth, new Buffer(payload)) :
+                                   encryptOld(userPublicKey, new Buffer(payload));
+
       options.headers = {
         'Content-Length': encrypted.cipherText.length,
         'Content-Type': 'application/octet-stream',
-        'Encryption-Key': 'keyid=p256dh;dh=' + urlBase64.encode(encrypted.localPublicKey),
         'Encryption': 'keyid=p256dh;salt=' + urlBase64.encode(encrypted.salt),
-        'Content-Encoding': 'aesgcm128',
       };
+
+      var cryptoHeader = 'keyid=p256dh;dh=' + urlBase64.encode(encrypted.localPublicKey);
+
+      if (useNewStandard) {
+        options.headers['Crypto-Key'] = cryptoHeader;
+        options.headers['Content-Encoding'] = 'aesgcm';
+      } else {
+        options.headers['Encryption-Key'] = cryptoHeader;
+        options.headers['Content-Encoding'] = 'aesgcm128';
+      }
     }
 
     var gcmPayload;
-    if (endpoint.indexOf('https://android.googleapis.com/gcm/send') === 0) {
-      if (payload) {
-        reject(new WebPushError('Payload not supported with GCM'));
-        return;
-      }
-
+    if (isGCM) {
       if (!gcmAPIKey) {
         console.warn('Attempt to send push notification to GCM endpoint, but no GCM key is defined'.bold.red);
       }
 
       var endpointSections = endpoint.split('/');
       var subscriptionId = endpointSections[endpointSections.length - 1];
-      gcmPayload = JSON.stringify({
+
+      var gcmObj = {
         registration_ids: [ subscriptionId ],
-      });
+      };
+      if (encrypted) {
+        gcmObj['raw_data'] = urlBase64.encode(encrypted.cipherText);
+      }
+      gcmPayload = JSON.stringify(gcmObj);
+
       options.path = options.path.substring(0, options.path.length - subscriptionId.length - 1);
 
       options.headers['Authorization'] = 'key=' + gcmAPIKey;
@@ -102,7 +146,7 @@ function sendNotification(endpoint, TTL, userPublicKey, payload) {
       options.headers['TTL'] = 2419200; // Default TTL is four weeks.
     }
 
-    var expectedStatusCode = gcmPayload ? 200 : 201;
+    var expectedStatusCode = isGCM ? 200 : 201;
     var pushRequest = https.request(options, function(pushResponse) {
       var body = "";
 
@@ -119,12 +163,12 @@ function sendNotification(endpoint, TTL, userPublicKey, payload) {
       });
     });
 
-    if (typeof payload !== 'undefined') {
+    if (isGCM) {
+      pushRequest.write(gcmPayload);
+    } else if (typeof payload !== 'undefined') {
       pushRequest.write(encrypted.cipherText);
     }
-    if (gcmPayload) {
-      pushRequest.write(gcmPayload);
-    }
+
     pushRequest.end();
 
     pushRequest.on('error', function(e) {
