1757e116d82fed9d8f7ecb1b1db3f24ea7503bf6
Francisco Jordano <arcturus@ardeenelinfierno.com>
2016-03-23 13:10:26 +0000

    Making the vapi and userauth parameters optional

--- bin/web-push.js
+++ bin/web-push.js
@@ -4,7 +4,7 @@ webPush.setGCMAPIKey(process.env.GCM_API_KEY);
 
 const argv = require('minimist')(process.argv.slice(2));
 
-const usage = 'Use: web-push --endpoint=<url> --key=<browser key> [--auth=<auth secret>] [--ttl=<seconds>] [--payload=<message>]';
+const usage = 'Use: web-push --endpoint=<url> --key=<browser key> [--auth=<auth secret>] [--ttl=<seconds>] [--payload=<message>] [--vapid]';
 
 if (!argv['endpoint'] || !argv['key']) {
   console.log(usage);
@@ -16,12 +16,14 @@ const key = argv['key'];
 const ttl = argv['ttl'] || 0;
 const payload = argv['payload'] || '';
 const auth = argv['auth'] || null;
+const useVAPID = argv['vapid'] || false;
 
-// Uses old sendMessage API
-var pushResult = null;
-if (!auth) {
-  pushResult = webPush.sendNotification(endpoint, ttl, key, payload)
-} else {
+var params = {
+  TTL: ttl,
+  payload,
+  userPublicKey: key
+};
+if (useVAPID) {
   const vapidKeys = webPush.generateVAPIDKeys();
   const vapid = {
     audience: 'https://www.mozilla.org/',
@@ -29,18 +31,15 @@ if (!auth) {
     privateKey: vapidKeys.privateKey,
     publicKey: vapidKeys.publicKey,
   };
-  pushResult = webPush.sendNotification(endpoint, {
-    TTL: ttl,
-    payload,
-    userPublicKey: key,
-    userAuth: auth,
-    vapid
-  });
+  params['vapid'] = vapid;
 }
-pushResult.then(() => {
+if (auth) {
+  params['userAuth'] = auth;
+}
+webPush.sendNotification(endpoint, params).then(() => {
   console.log('Push message sent.');
 }, (err) => {
   console.log('Error sending push message: ', err);
 }).then(() => {
   process.exit(0);
-})
+});
