32af3821eb52232e91d63660e7a647a1884405f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-03-23 16:50:01 +0100

    Make it possible to define VAPID keys to use during tests via env variables

--- test/testSelenium.js
+++ test/testSelenium.js
@@ -14,6 +14,12 @@ if (!process.env.GCM_API_KEY) {
   console.log('You need to set the GCM_API_KEY env variable to run the tests with Chromium.'.bold.red);
 }
 
+if (!process.env.VAPID_PRIVATE_KEY || !process.env.VAPID_PUBLIC_KEY) {
+  var keys = webPush.generateVAPIDKeys();
+  process.env.VAPID_PRIVATE_KEY = keys.privateKey.toString('base64');
+  process.env.VAPID_PUBLIC_KEY = keys.publicKey.toString('base64');
+}
+
 process.env.PATH = process.env.PATH + ':test_tools/';
 
 suite('selenium', function() {
@@ -177,12 +183,11 @@ suite('selenium', function() {
     });
   });
 
-  var vapidKeys = webPush.generateVAPIDKeys();
   var vapidParam = {
     audience: 'https://www.mozilla.org/',
     subject: 'mailto:web-push@mozilla.org',
-    privateKey: vapidKeys.privateKey,
-    publicKey: vapidKeys.publicKey,
+    privateKey: new Buffer(process.env.VAPID_PRIVATE_KEY, 'base64'),
+    publicKey: new Buffer(process.env.VAPID_PUBLIC_KEY, 'base64'),
   };
 
   test('send/receive notification without payload with Firefox Release', function() {
