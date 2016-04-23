383d9685bb1aee49d0f8b88015d2623ae4f9f538
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-04-23 16:06:02 +0100

    Remove test/server.js and move its code to testSelenium.js

--- test/testSelenium.js
+++ test/testSelenium.js
@@ -1,3 +1,4 @@
+var webPush = require('../index');
 var assert = require('assert');
 var fs = require('fs');
 var path = require('path');
@@ -5,13 +6,15 @@ var fse = require('fs-extra');
 var temp = require('temp').track();
 var colors = require('colors');
 var childProcess = require('child_process');
+var http = require('http');
+var portfinder = require('portfinder');
 var net = require('net');
 var seleniumInit = require('./selenium-init');
-var createServer = require('./server');
-var webPush = require('../index.js');
 
 if (!process.env.GCM_API_KEY) {
   console.log('You need to set the GCM_API_KEY env variable to run the tests with Chromium.'.bold.red);
+} else {
+  webPush.setGCMAPIKey(process.env.GCM_API_KEY);
 }
 
 if (!process.env.VAPID_PRIVATE_KEY || !process.env.VAPID_PUBLIC_KEY) {
@@ -23,6 +26,91 @@ if (!process.env.VAPID_PRIVATE_KEY || !process.env.VAPID_PUBLIC_KEY) {
 }
 
 process.env.PATH = process.env.PATH + ':test_tools/';
+
+function createServer(pushPayload, vapid) {
+  var server = http.createServer(function(req, res) {
+    if (req.method === 'GET') {
+      if (req.url === '/') {
+        req.url = '/index.html';
+      }
+
+      if (!fs.existsSync('test' + req.url)) {
+        res.writeHead(404);
+        res.end(data);
+        return;
+      }
+
+      var data = fs.readFileSync('test' + req.url);
+
+      res.writeHead(200, {
+        'Content-Length': data.length,
+        'Content-Type': path.extname(req.url) === '.html' ? 'text/html' : 'application/javascript',
+      });
+
+      res.end(data);
+    } else {
+      var body = '';
+
+      req.on('data', function(chunk) {
+        body += chunk;
+      })
+
+      req.on('end', function() {
+        var obj = JSON.parse(body);
+
+        console.log('Push Application Server - Register: ' + obj.endpoint);
+
+        console.log('Push Application Server - Send notification to ' + obj.endpoint);
+
+        var promise;
+        if (!pushPayload) {
+          promise = webPush.sendNotification(obj.endpoint, {
+            vapid: vapid,
+          });
+        } else {
+          promise = webPush.sendNotification(obj.endpoint, {
+            payload: pushPayload,
+            userPublicKey: obj.key,
+            userAuth: obj.auth,
+            vapid: vapid,
+          });
+        }
+
+        promise
+        .then(function() {
+          console.log('Push Application Server - Notification sent to ' + obj.endpoint);
+        })
+        .catch(function(error) {
+          console.log('Push Application Server - Error in sending notification to ' + obj.endpoint);
+          console.log(error);
+        })
+      });
+
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      });
+
+      res.end('ok');
+    }
+  });
+
+  portfinder.getPort(function(err, port) {
+    if (err) {
+      server.port = 50005;
+    } else {
+      server.port = port;
+    }
+    server.listen(server.port);
+  });
+
+  return new Promise(function(resolve, reject) {
+    server.on('listening', function() {
+      resolve(server);
+    });
+  });
+}
 
 function isPortOpen(port) {
   return new Promise(function(resolve, reject) {
