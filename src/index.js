9ba46bab8b29eb36a044e39b5190bf2ef9aa89bc
Julian Grinblat <julian@dotcore.co.il>
2017-02-09 10:56:56 +0900

    Expose WebPushError as a public API

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@
 const vapidHelper = require('./vapid-helper.js');
 const encryptionHelper = require('./encryption-helper.js');
 const WebPushLib = require('./web-push-lib.js');
+const WebPushError = require('./web-push-error.js');
 
 const webPush = new WebPushLib();
 
 module.exports = {
+  WebPushError: WebPushError,
   encrypt: encryptionHelper.encrypt,
   getVapidHeaders: vapidHelper.getVapidHeaders,
   generateVAPIDKeys: vapidHelper.generateVAPIDKeys,
