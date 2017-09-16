b56c83e639d85b038db5d57c89dd12770f7e34df
Matt Gaunt <matt@gauntface.co.uk>
2017-09-15 17:13:27 -0700

    Adding browser download retries (#294)
    * Adding browser download retries
    
    * Removing change to forceDownload
    
    * Adding attempt default
    
    * Fixing lint errors
    
    * Fixing for node 4

--- test/helpers/download-test-browsers.js
+++ test/helpers/download-test-browsers.js
@@ -2,18 +2,39 @@
 
 const seleniumAssistant = require('selenium-assistant');
 
+const MAX_RETRIES = 3;
 let forceDownload = false;
 if (process.env.TRAVIS) {
   forceDownload = true;
 }
 
+const downloadBrowser = (name, version, attempt) => {
+  attempt = attempt || 0;
+
+  return new Promise((resolve, reject) => {
+    seleniumAssistant.downloadBrowser(name, version, forceDownload)
+    .catch((err) => {
+      if (attempt < MAX_RETRIES) {
+        console.log(`Attempt ${attempt + 1} of browser ${name} - ${version} failed.`);
+        return downloadBrowser(name, version, attempt + 1);
+      }
+
+      return reject(err);
+    })
+    .then(() => {
+      console.log(`Successfully downloaded ${name} - ${version}.`);
+      resolve();
+    });
+  });
+};
+
 const promises = [
-  seleniumAssistant.downloadBrowser('firefox', 'stable', forceDownload),
-  seleniumAssistant.downloadBrowser('firefox', 'beta', forceDownload),
-  seleniumAssistant.downloadBrowser('firefox', 'unstable', forceDownload),
-  seleniumAssistant.downloadBrowser('chrome', 'stable', forceDownload),
-  seleniumAssistant.downloadBrowser('chrome', 'beta', forceDownload),
-  seleniumAssistant.downloadBrowser('chrome', 'unstable', forceDownload)
+  downloadBrowser('firefox', 'stable'),
+  downloadBrowser('firefox', 'beta'),
+  downloadBrowser('firefox', 'unstable'),
+  downloadBrowser('chrome', 'stable'),
+  downloadBrowser('chrome', 'beta'),
+  downloadBrowser('chrome', 'unstable')
 ];
 
 Promise.all(promises)
