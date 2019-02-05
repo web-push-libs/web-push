83aad88e454eb74eeb5908926dde89e27b89f35e
Sarpdoruk Tahmaz <sarpdoruk@gmail.com>
2019-02-05 17:28:59 +0300

    FCM endpoint support for GCM/FCM projects
    Signed-off-by: Sarpdoruk Tahmaz <sarpdoruk@gmail.com>

--- src/web-push-lib.js
+++ src/web-push-lib.js
@@ -220,16 +220,9 @@ WebPushLib.prototype.generateRequestDetails =
     }
 
     const isGCM = subscription.endpoint.indexOf('https://android.googleapis.com/gcm/send') === 0;
+    const isFCM = subscription.endpoint.indexOf('https://fcm.googleapis.com/fcm/send') === 0;
     // VAPID isn't supported by GCM hence the if, else if.
-    if (isGCM) {
-      if (!currentGCMAPIKey) {
-        console.warn('Attempt to send push notification to GCM endpoint, ' +
-          'but no GCM key is defined. Please use setGCMApiKey() or add ' +
-          '\'gcmAPIKey\' as an option.');
-      } else {
-        requestDetails.headers.Authorization = 'key=' + currentGCMAPIKey;
-      }
-    } else if (currentVapidDetails) {
+    if (currentVapidDetails) {
       const parsedUrl = url.parse(subscription.endpoint);
       const audience = parsedUrl.protocol + '//' +
         parsedUrl.host;
@@ -251,6 +244,14 @@ WebPushLib.prototype.generateRequestDetails =
         } else {
           requestDetails.headers['Crypto-Key'] = vapidHeaders['Crypto-Key'];
         }
+      }
+    } else if (isGCM || isFCM) {
+      if (!currentGCMAPIKey) {
+        console.warn('Attempt to send push notification to GCM endpoint, ' +
+          'but no GCM key is defined. Please use setGCMApiKey() or add ' +
+          '\'gcmAPIKey\' as an option.');
+      } else {
+        requestDetails.headers.Authorization = 'key=' + currentGCMAPIKey;
       }
     }
 
