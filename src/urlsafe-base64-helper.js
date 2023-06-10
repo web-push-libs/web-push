b10a9c0bfd27f7df1403c72e2cfb7d8fc7b83ff7
haithem <haithem2001@gmail.com>
2023-06-10 22:53:01 +0100

    decode fix

--- src/urlsafe-base64-helper.js
+++ src/urlsafe-base64-helper.js
@@ -4,26 +4,26 @@
 
 function encode(buffer) {
    return buffer.toString('base64')
-    .replace(/\+/g, '-') // Convert '+' to '-'
-    .replace(/\//g, '_') // Convert '/' to '_'
-    .replace(/=+$/, ''); // Remove ending '='
+      .replace(/\+/g, '-') // Convert '+' to '-'
+      .replace(/\//g, '_') // Convert '/' to '_'
+      .replace(/=+$/, ''); // Remove ending '='
 }
 
 function decode(base64) {
    // Add removed at end '='
-  base64 += Array(5 - (base64.length % 4)).join('=');
+   base64 += Array(5 - (base64.length % 4)).join('=');
 
-  base64 = base64
-    .replace(/-/g, '+') // Convert '-' to '+'
-    .replace(/_/g, '/'); // Convert '_' to '/'
+   base64 = base64
+      .replace(/-/g, '+') // Convert '-' to '+'
+      .replace(/_/g, '/'); // Convert '_' to '/'
 
-  // change from urlsafe-base64 since new Buffer() is deprecated
-  return Buffer.from(base64).toString('base64');
+   // change from urlsafe-base64 since new Buffer() is deprecated
+   return Buffer.from(base64, 'base64');
 }
 
 function validate(base64) {
    return /^[A-Za-z0-9\-_]+$/.test(base64);
- }
+}
 
 module.exports = {
    encode: encode,
