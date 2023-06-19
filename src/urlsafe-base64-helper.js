921c73a184d3696ab8c33df1da6f7d7fa608f84f
Niklas Mollenhauer <nikeee@outlook.com>
2023-06-19 23:37:09 +0200

    Add type annotations

--- src/urlsafe-base64-helper.js
+++ src/urlsafe-base64-helper.js
@@ -2,17 +2,29 @@
 
 'use strict';
 
+/**
+ * @param {Buffer} buffer
+ * @returns {string}
+ */
 function encode(buffer) {
    return buffer.toString('base64url');
 }
 
+/**
+ * @param {string} base64
+ * @returns {Buffer}
+ */
 function decode(base64) {
-  return Buffer.from(base64, 'base64url');
+   return Buffer.from(base64, 'base64url');
 }
 
+/**
+ * @param {string} base64
+ * @returns {boolean}
+ */
 function validate(base64) {
    return /^[A-Za-z0-9\-_]+$/.test(base64);
- }
+}
 
 module.exports = {
    encode: encode,
