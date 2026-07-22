a7d9fae58a995acf5ae82e1f7722be2514bcf5da
James Garbutt <43081j@users.noreply.github.com>
2026-07-22 22:25:44 +0100

    refactor: move to classes rather than prototypes (#966)
    This switches the push lib class to be an actual class, and also
    switches the `WebPushError` to be a regular extension of `Error`.

--- src/web-push-error.js
+++ src/web-push-error.js
@@ -1,14 +1,11 @@
-import { inherits } from 'node:util';
+export class WebPushError extends Error {
+  constructor(message, statusCode, headers, body, endpoint) {
+    super(message);
 
-export function WebPushError(message, statusCode, headers, body, endpoint) {
-  Error.captureStackTrace(this, this.constructor);
-
-  this.name = this.constructor.name;
-  this.message = message;
-  this.statusCode = statusCode;
-  this.headers = headers;
-  this.body = body;
-  this.endpoint = endpoint;
+    this.name = 'WebPushError';
+    this.statusCode = statusCode;
+    this.headers = headers;
+    this.body = body;
+    this.endpoint = endpoint;
+  }
 }
-
-inherits(WebPushError, Error);
